301f7249266626ac5f14ceccb5a1c0da7f56ae21
Lucasbarrost <lucasbarrost9@gmail.com>
2024-08-09 11:14:41 -0300

    Add files via upload

---
+++ ProjetoMario/index.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+const button = document.getElementById('button')
+const navigation = document.querySelector('.navigation')
+
+function handleClick() {
+  button.classList.toggle('active')
+  navigation.classList.toggle('active')
+=======
+const button = document.getElementById('button')
+const navigation = document.querySelector('.navigation')
+
+function handleClick() {
+  button.classList.toggle('active')
+  navigation.classList.toggle('active')
+>>>>>>> 658d5ae7c0977bbadeb908325c94619e7b8efef7
+}
